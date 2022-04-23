b8d94c450d5328b9e44db5f400cc188892ace005
i-spm <shivspm54@gmail.com>
2022-04-23 20:16:41 +0530

    initial commit

---
+++ src/main/java/com/example/springbootcrudrestfulwebdifferentmappings/SpringbootCrudRestfulWebDifferentmappingsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootcrudrestfulwebdifferentmappings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCrudRestfulWebDifferentmappingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCrudRestfulWebDifferentmappingsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootcrudrestfulwebdifferentmappings/SpringbootCrudRestfulWebDifferentmappingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootcrudrestfulwebdifferentmappings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCrudRestfulWebDifferentmappingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
